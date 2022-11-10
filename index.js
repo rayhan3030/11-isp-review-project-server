1a395dd286cca7f3d24c5afa38a3bcad44d6128a
rayhan3030 <rayhanddd@gmail.com>
2022-11-10 15:41:08 +0600

    home page 3 card show api created

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hqp6ngz.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('ispReview').collection('services');
+        // this is review collection database
+        const reviewCollection = client.db('ispReview').collection('reviews');
+
+        //service api ---------------------------------------------------------------------------------------------------
+        // this api for homepage 3 card
+        app.get('/services3', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
